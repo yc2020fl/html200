ee717223fe1c6c34545aff4030af6abef199060b
YanYi Chen <yanyichen@YanYis-MacBook.local>
2021-05-11 23:50:48 -0700

    added new lesson04 folder

---
+++ lesson04/alertfunction.js
@@ -0,0 +1,11 @@
+
+
+function numberSqure () {
+  let input = prompt ('enter a number');
+  let square = Number(input);
+  let result = 0;
+
+  result = square  * Number(input);
+
+  alert(result);
+}
